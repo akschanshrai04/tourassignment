452660c6edf323824d8332e6a129b14ce1268158
akschanshrai04 <akschanshrai97@gmail.com>
2024-12-17 23:10:10 +0530

    cors-check

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(cors());
 app.use(
     cors({
-        origin: "http://localhost:5173", 
+        origin: "*", 
         methods: ["GET", "POST", "PUT", "DELETE"], 
         credentials: true, 
     })
